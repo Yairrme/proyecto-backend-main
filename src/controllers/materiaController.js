513a057aea8c9d77ea9ae5ced93f7e46ba1d3a4f
Francoseguel34 <francoseguel010@gmail.com>
2025-06-30 01:38:18 -0300

    Update materiaController.js

--- src/controllers/materiaController.js
+++ src/controllers/materiaController.js
@@ -7,22 +7,37 @@ exports.getMaterias = (req, res) => {
   });
 };
 
-exports.createMateria = (req, res) => {
-  const { nombre, profesorId } = req.body;
-  db.query('INSERT INTO materias (nombre, profesorId) VALUES (?, ?)', [nombre, profesorId], (err, result) => {
+exports.getMateria = (req, res) => {
+  const id = req.params.id;
+  db.query('SELECT * FROM materias WHERE id = ?', [id], (err, result) => {
     if (err) return res.status(500).send(err);
-    res.json({ message: 'Materia creada', id: result.insertId });
+    if (result.length === 0) return res.status(404).json({ message: 'Materia no encontrada' });
+    res.json(result[0]);
   });
 };
 
-exports.getAlumnosPorMateria = (req, res) => {
+exports.createMateria = (req, res) => {
+  const { nombre, profesorId } = req.body;
+  db.query(
+    'INSERT INTO materias (nombre, profesorId) VALUES (?, ?)',
+    [nombre, profesorId || null],
+    (err, result) => {
+      if (err) return res.status(500).send(err);
+      res.json({ message: 'Materia creada', id: result.insertId });
+    }
+  );
+};
+
+exports.getAlumnosporMateria = (req, res) => {
   const materiaId = req.params.id;
+
   const sql = `
     SELECT a.id, a.nombre, a.email
     FROM alumnos a
     INNER JOIN matriculas m ON a.id = m.alumnoId
     WHERE m.materiaId = ?
   `;
+
   db.query(sql, [materiaId], (err, result) => {
     if (err) return res.status(500).send(err);
     res.json(result);
